2b0f104f4d39db6f5cefc012596a59f4510bae96
Vashisht Madhavan <vashishtmadhavan@Vashs-MacBook-Pro.local>
2023-04-04 23:13:26 -0400

    adding docstring

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from langchain.document_loaders import DirectoryLoader, PyPDFLoader
@@ -6,26 +7,39 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from models.retrieval import RetrievalModel
 from vector_store import VectorStoreWrapper
 
-loader = DirectoryLoader(path="examples/", glob="*.pdf", loader_cls=PyPDFLoader)
-faiss_path = "examples_index/"
+FAISS_PATH = "examples_index"
+
+loader = DirectoryLoader(
+    path="examples/", glob="*.pdf", loader_cls=PyPDFLoader)
 docs = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000,  separators=["\n \n","\n\n", "\n", " ", ""])
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=3000,  separators=["\n \n", "\n\n", "\n", " ", ""])
 split_docs = text_splitter.split_documents(docs)
-db = VectorStoreWrapper(load_path=faiss_path).get_index(split_docs)
+db = VectorStoreWrapper(load_path=FAISS_PATH).get_index(split_docs)
 
 app = FastAPI()
 
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with your frontend app's URL
+    # Replace with your frontend app's URL
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get("/pdf")
-def get_pdf(url: str):
+def get_pdf(url: str) -> Response:
+    """Serve a PDF file from a URL.
+
+    Args:
+        url (str): The url of the PDF file.
+
+    Returns:
+        response: File response for Fast API.
+    """
     filename = url.split("/")[-1]
     with open(url, "rb") as f:
         pdf = f.read()
@@ -35,7 +49,15 @@ def get_pdf(url: str):
 
 
 @app.get("/search")
-def qa(query: str):
+def search(query: str) -> Dict[str, Any]:
+    """Run retrieval model on a query.
+
+    Args:
+        query (str): The question to ask the LLM
+
+    Returns:
+        response: File response for Fast API.
+    """
     qa = RetrievalModel(retriever=db.as_retriever(search_kwargs={"k": 5}),
                         with_sources=True)
     result = qa.run(query)
